e8f3e457f39aaf4600d496ade64615d7bcfe3737
hu.kai@datasw.com <hukai,,147>
2020-03-25 17:20:15 +0800

    加入springboot

---
+++ api/src/main/java/com/ttxs/Application.java
@@ -0,0 +1,15 @@
+package com.ttxs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kai.hu
+ * @date 2020/3/25 17:03
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
--- api/src/main/java/com/ttxs/controller/TestController.java
+++ api/src/main/java/com/ttxs/controller/TestController.java
@@ -1,14 +1,24 @@
 package com.ttxs.controller;
 
 import com.ttxs.utils.ArithmeticUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author kai.hu
  * @date 2020/3/23 11:35
  */
+@RestController
+@RequestMapping("test")
+@Api(tags = "test")
 public class TestController {
-    public static void main(String[] args) {
-        int i = 1534236469;
-        System.out.println(ArithmeticUtil.reversNumber(i));
+
+    @ApiOperation("反转字符串")
+    @RequestMapping("reverseInt")
+    public String reverseInt(Integer num) {
+        return ArithmeticUtil.reversNumber(num) + "";
     }
+
 }
